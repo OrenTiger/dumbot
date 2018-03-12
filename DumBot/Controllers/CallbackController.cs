18896563687653d7241c347a881d16bba779a6ee
Timur Basyrov <t.basyrov@inostudio.com>
2018-03-12 16:41:10 +0500

    Add CallbackController #1

---
+++ DumBot/Controllers/CallbackController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using DumBot.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace DumBot.Controllers
+{
+    [Route("[controller]")]
+    public class CallbackController : Controller
+    {
+        public IConfiguration Configuration { get; }
+
+        private readonly int _serverConfirmationGroupId;
+        private readonly string _serverConfirmationReplyString;
+        
+        public CallbackController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            _serverConfirmationGroupId = int.Parse(Configuration["ServerConfirmationGroupId"]);
+            _serverConfirmationReplyString = Configuration["ServerConfirmationReplyString"];
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        public string Post([FromBody]CallbackEventModel callbackEvent)
+        {
+            if (string.IsNullOrEmpty(callbackEvent.Type))
+            {
+                HttpContext.Response.StatusCode = 400;
+                return string.Empty;
+            }
+
+            switch (callbackEvent.Type)
+            {
+                case CallbackEventType.Confirmation:
+                    return callbackEvent.Group_id == _serverConfirmationGroupId
+                        ? _serverConfirmationReplyString
+                        : string.Empty;
+                default:
+                    return "ok";
+            }
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DumBot/Models/CallbackEventModel.cs
@@ -0,0 +1,8 @@
+﻿namespace DumBot.Models
+{
+    public class CallbackEventModel
+    {
+        public string Type { get; set; }
+        public int Group_id { get; set; }
+    }
+}
